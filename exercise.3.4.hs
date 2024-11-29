5900032eda07ffca15bded5b308295e39bc93467
charltonaustin <charlton.austin@gmail.com>
2024-11-29 11:13:47 -0800

    Exercise 3.4

---
+++ exercise.3.4.hs
@@ -0,0 +1,9 @@
+bag2Fee :: Bool -> Int
+bag2Fee hasABag = if hasABag then 100 else 0
+
+bag2Fee' :: Bool -> Int
+bag2Fee' hasABag = case hasABag of
+                       True -> 100
+                       False -> 0
+
+
