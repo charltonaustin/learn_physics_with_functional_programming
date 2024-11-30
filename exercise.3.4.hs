be20126d6c3f10f91b24136d04fa0dc711b8bb8e
charltonaustin <charlton.austin@gmail.com>
2024-11-30 12:44:53 -0800

    Using hindent on exercise 3.4

--- exercise.3.4.hs
+++ exercise.3.4.hs
@@ -1,9 +1,11 @@
 bag2Fee :: Bool -> Int
-bag2Fee hasABag = if hasABag then 100 else 0
+bag2Fee hasABag =
+  if hasABag
+    then 100
+    else 0
 
 bag2Fee' :: Bool -> Int
-bag2Fee' hasABag = case hasABag of
-                       True -> 100
-                       False -> 0
-
-
+bag2Fee' hasABag =
+  case hasABag of
+    True -> 100
+    False -> 0
