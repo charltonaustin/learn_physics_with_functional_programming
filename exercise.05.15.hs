e3d7f330f671426e5524113f67a1402d9c7cd711
charltonaustin <charlton.austin@gmail.com>
2024-12-19 17:49:58 -0800

    Exercise 5.15 - part 2

--- exercise.05.15.hs
+++ exercise.05.15.hs
@@ -1,5 +1,11 @@
+factorial :: Double -> Double
+factorial n = product [1..n]
+
+expApprox :: Double -> Double -> Double
+expApprox x n = sum [(x ** m) / factorial m | m <- [0..n]]
+
 expSeries :: Double -> [Double]
-expSeries x = repeat (exp x)
+expSeries x = [expApprox x m | m <- [0, 1..]]
 
 diff :: Double -> Double -> Double
 diff a b = abs(a - b)
@@ -18,3 +24,6 @@ takeTillClose x = takeWhile (greaterThan1PercentDiff x) (expSeries x)
 
 howBig :: Double -> Int
 howBig x = length (takeTillClose x)
+
+howBig 1 -- 4
+howBig 10 -- 29
