78c3ef8a75ca2bce1637244e04dec7bd81f97f99
charltonaustin <charlton.austin@gmail.com>
2024-12-21 13:03:30 -0800

    Exercise 6.01 part 2

--- exercise.06.01.hs
+++ exercise.06.01.hs
@@ -5,6 +5,10 @@ type TimeInterval = R
 type Position     = R
 type Velocity     = R
 
+type Integration = (R -> R) -> R
+
+integral :: R -> Integration
+integral dt f a b = sum [ f t * dt | t <- [a + dt/2, a + 3 * dt/2.. b - dt/2]]
 -- A function that takes an initial velocity and returns a function
 -- that takes in time and returns a height
 yRock :: R -> R-> R
