3b1f713e4ce222681d2e6212bd13128a7e8c1df7
charltonaustin <charlton.austin@gmail.com>
2024-12-20 20:55:51 -0800

    Exercise 6.01 part 1

---
+++ exercise.06.01.hs
@@ -0,0 +1,17 @@
+type R = Double
+
+type Time         = R
+type TimeInterval = R
+type Position     = R
+type Velocity     = R
+
+-- A function that takes an initial velocity and returns a function
+-- that takes in time and returns a height
+yRock :: R -> R-> R
+yRock = undefined
+
+
+-- A function that takes an initial velocity and returns a function
+-- that takes in time and returns a velocity
+vRock :: R -> R -> R
+vRock = undefined
