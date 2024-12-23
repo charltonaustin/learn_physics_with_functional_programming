bbd7a198c77bb1d007e289b6bd7820a939904d6a
charltonaustin <charlton.austin@gmail.com>
2024-12-22 16:16:50 -0800

    Exercise 6.01 part 3

--- exercise.06.01.hs
+++ exercise.06.01.hs
@@ -5,17 +5,23 @@ type TimeInterval = R
 type Position     = R
 type Velocity     = R
 
-type Integration = (R -> R) -> R
+type Integration = (R -> R) -- function
+                 -> R       -- lower limit
+                 -> R       -- upper limit
+                 -> R       -- result
+
 
 integral :: R -> Integration
-integral dt f a b = sum [ f t * dt | t <- [a + dt/2, a + 3 * dt/2.. b - dt/2]]
+integral dt f a b = sum [ f t * dt | t <- [a + dt/2, a + 3 * dt/2 .. b - dt/2]]
+
+
 -- A function that takes an initial velocity and returns a function
 -- that takes in time and returns a height
 yRock :: R -> R-> R
-yRock = undefined
+yRock initialV = integral 0.01 (vRock initialV) 0
 
 
 -- A function that takes an initial velocity and returns a function
 -- that takes in time and returns a velocity
-vRock :: R -> R -> R
-vRock = undefined
+vRock :: R -> (R -> R)
+vRock initialV = \t -> initialV + (integral 0.1 (const (-9.8)) 0 t)
