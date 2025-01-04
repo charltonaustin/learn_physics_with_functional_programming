fba9a321da9e1629c425d37309aea3bb967c46cc
charltonaustin <charlton.austin@gmail.com>
2025-01-04 15:01:41 -0800

    Fixing exercise 09.10

--- exercise.09.10.hs
+++ exercise.09.10.hs
@@ -1,6 +1,9 @@
 type R = Double
+
+initialV = 0.0
+
 tvPairs :: [(R, R)]
-tvPairs = iterate tvUpdate (0, 0)
+tvPairs = iterate tvUpdate (0, initialV)
 
 tvUpdate :: (R, R) -> (R, R)
-tvUpdate (t, v) = (t + 1, v + 5 * t )
+tvUpdate (t, v) = (t + 1, initialV + 5 * (t + 1) )
