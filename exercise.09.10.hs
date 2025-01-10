5c7a57ca5d31b35d0628656aa1365aa505664b64
charltonaustin <charlton.austin@gmail.com>
2025-01-09 16:50:48 -0800

    Exercise 09.10 - implementation with integration

--- exercise.09.10.hs
+++ exercise.09.10.hs
@@ -1,9 +1,10 @@
 type R = Double
 
 initialV = 0.0
-
+a = const 5
+stepSize = 1
 tvPairs :: [(R, R)]
 tvPairs = iterate tvUpdate (0, initialV)
 
 tvUpdate :: (R, R) -> (R, R)
-tvUpdate (t, v) = (t + 1, initialV + 5 * (t + 1) )
+tvUpdate (t, v) = (t + stepSize, v + a(stepSize/2) * stepSize)
