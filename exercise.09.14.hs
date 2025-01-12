406dfe54c395323b7d5902ace877c31944402a57
charltonaustin <charlton.austin@gmail.com>
2025-01-11 16:51:24 -0800

    Exercise 09.14 - implementation with integration

--- exercise.09.14.hs
+++ exercise.09.14.hs
@@ -1,22 +1,17 @@
 type R = Double
 
-a = -9.8
+a = const (-9.8)
 initialV = 15.0
 initialS = 0.0
 stepSize = 1.0
 
-createUpdateV :: R -> R
-createUpdateV = \t -> (initialV + a * t)
 
-createUpdateS = \t -> initialS + initialV * t + 0.5 * a * t ** 2
+calculateV stepSize v = v + a(stepSize/2.0) * stepSize
+calculateS stepSize s v = s + calculateV(stepSize/2.0) v * stepSize
 
-updateV :: R -> R
-updateV t = createUpdateV t
-
-updateS :: R -> R
-updateS t = createUpdateS t
 
 takeAStep  (t, s, v) = let nextT = t + stepSize
-                           nextV = updateV nextT
-                           in (nextT, updateS nextT, nextV)
+                           nextV = calculateV stepSize v
+                           nextS = calculateS stepSize s v
+                           in (nextT, nextS, nextV)
 timePositionVelocity = iterate takeAStep (0, initialS, initialV)
