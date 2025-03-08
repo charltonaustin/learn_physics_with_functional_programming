181406f66d78c47006b2b6ed824c0710f8a41ec0
charltonaustin <charlton.austin@gmail.com>
2025-03-08 07:45:27 -0700

    Working on Ch 12

---
+++ chapter_12/TrajectoryStack/app/MakeTrajectoryGraph.hs
@@ -0,0 +1,18 @@
+module Main (main) where
+
+import SimpleVec (iHat, kHat, xComp, zComp, projectilePos, (^+^), (*^) )
+import Graphics.Gnuplot.Simple (Attribute(..), plotPath)
+
+main :: IO()
+main =
+  let posInitial = 10 *^ kHat
+      velInitial = 20 *^ cos (pi/6) *^ iHat ^+^ 20 *^ sin (pi/6) *^ kHat
+      posFun = projectilePos posInitial velInitial
+      pairs = [(xComp r, zComp r) | t <- [0, 0.1 ..], let r = posFun t]
+      plottingPairs = takeWhile (\(_, z) -> z >= 0) pairs
+  in plotPath [Title "Projectile Motion"
+              ,XLabel "Horizontal Position (m)"
+              ,YLabel "Verticle Position (m)"
+              ,PNG "projectile.png"
+              ,Key Nothing
+              ] plottingPairs
